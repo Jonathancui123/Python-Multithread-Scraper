00c636f37aa3266e6f03fda17b1b8d2a5187fd1d
Jonathan Cui <jonathancui123@gmail.com>
2020-07-29 00:14:48 -0400

    done selenium, working on parsing

---
+++ getPopularLinks.py
@@ -0,0 +1,29 @@
+import datetime
+import string
+from time import sleep, time
+from bs4 import BeautifulSoup
+from scraper import getDriver, processPage
+
+#Track elapsesd time throughout the project
+
+# 3. Parse HTML (capture info from each page -- given an html file)
+def parseUDPopularHTML(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    print(soup.find(id="columnist").prettify())
+
+if __name__ == '__main__':
+    # 2. Handler script set up browser, to decide which pages to run the function on, track elapsed time, open and close files, 
+    browser = getDriver()
+    baseURL = "https://www.urbandictionary.com/popular.php?character="
+    output_timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    output_filename = f'output_{output_timestamp}.csv'
+    startTime = time()
+    failedURLs = []
+    for char in string.ascii_uppercase:
+        print(f'Processing letter {char}')
+        processPage(browser, baseURL + char, output_filename, failedURLs, "columnist", parseUDPopularHTML)
+    browser.quit()
+    endTime = time()
+    elapsed_time = endTime - startTime
+    print(f'Elapsed time: {elapsed_time} with the following failures:')
+    print(failedURLs)
---
+++ scraper.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from time import sleep
+
+def getDriver():
+    # 1. Set up browser
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('log-level=3')
+    path = 'C:\\Users\\Jonathan Cui\\AppData\\Local\\ChromeWebdriver\\chromedriver.exe'
+    driver = webdriver.Chrome(executable_path=os.path.normpath(path), chrome_options=options)
+    return driver
+
+def connectPage(browser, URL, signalElemName):
+    # 2. Connect to page -- given browser instance and a page number/letter
+    attempts = 0
+    while attempts < 3:
+        # Try 3 times
+        try:
+            browser.get(URL)
+            # Wait until an element is present on the page
+            element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, signalElemName)))
+            return True
+        except:
+            # Handle exceptions and retry
+            attempts += 1
+            print(f'Exception connecting to page {URL}, attempt #{attempts}')
+            sleep(1)
+
+    return False
+
+# 3. Write to file given contents and a filename to write to 
+def writeToFile(contents, fileName):
+    print("writing to file")
+    return
+ 
+# 4 function to run on each page, write to files
+def processPage(browser, URL, outputFileName, failed, signalElemName, parser):
+    if connectPage(browser, URL, signalElemName):
+        sleep(2)
+        html = browser.page_source
+        output = parser(html)
+        writeToFile(output, outputFileName)
+    else:
+        print(f'ERROR: Failed to connect to {URL}')
+        failed.append(URL)
+
+
+
